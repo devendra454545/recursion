3a92b17e4a72b835ff0fd7b52d9e5f183281d201
Devendra Singh Thakur <imdevendra363@gmail.com>
2026-02-25 23:39:42 +0530

    Reversing an array using recursion

--- array_rev.py
+++ array_rev.py
@@ -2,12 +2,12 @@
 # 
 # def rev(arr):
 #     n = len(arr)//2
-#     for i in range(n+1):
-#         temp = arr[i]
-#         arr[i] = arr[len(arr)-i-1]
+#     for i in range(n+1):                    
+#         temp = arr[i]                     #<-----(Swapping without tuple unpacking)    
+#         arr[i] = arr[len(arr)-i-1]        #----->(Tuple unpacking arr[i],arr[len(arr)-i-1] = arr[len(arr)-i-1],arr[i] )
 #         arr[len(arr)-i-1] = temp
 #     return arr
-arr=[1,2,3,4,5,6]
+# arr=[1,2,3,4,5,6]
 # print(rev(arr))
 # 
 # Using reverse function
@@ -15,5 +15,5 @@ arr=[1,2,3,4,5,6]
 # print(arr)
 # 
 # Using slicing (array[start : stop : step])
-rev = arr[::-1]
-print(rev)
+# rev = arr[::-1]
+# print(rev)
